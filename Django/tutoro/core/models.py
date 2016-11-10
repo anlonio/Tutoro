022564c9fa0aacd1bcbea562fc0761f2f191f906
anlonio <anloniob@gmail.com>
2016-11-10 11:17:51 -0200

    modelo disciplina

--- Django/tutoro/core/models.py
+++ Django/tutoro/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Disciplina(models.Model):
+	professores = models.ManyToManyField(User)
+	nome = models.CharField(max_length=50)
+	sigla = models.CharField(max_length=10)
