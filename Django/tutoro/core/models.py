4008e1ede5d425d79bd1fcf9fae332446e010424
anlonio <anloniob@gmail.com>
2016-11-10 11:20:44 -0200

    modelo instituicao

--- Django/tutoro/core/models.py
+++ Django/tutoro/core/models.py
@@ -7,3 +7,11 @@ class Disciplina(models.Model):
 	professores = models.ManyToManyField(User)
 	nome = models.CharField(max_length=50)
 	sigla = models.CharField(max_length=10)
+
+class Instituicao(models.Model):
+	professsor = models.ManyToMany(User)
+	nome = models.CharField(max_length=150)
+	cidade = models.CharField(max_length=100)
+	uf = models.CharField(max_length=2, min_length=4)
+	sigla = models.CharField(max_length=10, min_length=2)
+	descricao = models.TextField(max_length=255)
